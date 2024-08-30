b43f761168977fd8ce797f320eb1a2bdca28533c
Max Pintchouk <maxpintchouk4321@gmail.com>
2024-08-30 17:17:11 -0400

    1.5

--- YouAreAwesomeApp/YouAreAwesomeApp/ContentView.swift
+++ YouAreAwesomeApp/YouAreAwesomeApp/ContentView.swift
@@ -8,14 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var messageString: String = ""
+    @State private var messageString: String = "When the genuis bar needs help, they call you"
     var body: some View {
         VStack {
             Text(messageString)
                 .font(.largeTitle)
                 .fontWeight(.heavy)
+                .minimumScaleFactor(0.5)
+                .multilineTextAlignment(.center)
                 .foregroundStyle(.red)
-                .italic()
+                .frame(height: 150)
+                .frame(maxWidth: .infinity)
+                .border(.orange, width: 1)
                 .padding()
             HStack {
                 Button("Awesome") {
@@ -27,6 +31,7 @@ struct ContentView: View {
                 }
                 .buttonStyle(.borderedProminent)
             }
+            .border(.purple, width: 5)
         }
     }
 }
