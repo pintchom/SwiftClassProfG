3e47042d9299d2b1e1c0a4a8fb676b8a15299b39
Max Pintchouk <maxpintchouk4321@gmail.com>
2024-09-02 13:30:20 -0400

    1.7

--- YouAreAwesomeApp/YouAreAwesomeApp/ContentView.swift
+++ YouAreAwesomeApp/YouAreAwesomeApp/ContentView.swift
@@ -10,40 +10,53 @@ import SwiftUI
 struct ContentView: View {
     @State private var messageString: String = "When the genuis bar needs help, they call you!"
     var body: some View {
-        VStack {
-            Text("You have Skills!")
-                .font(.largeTitle)
-                .fontWeight(.black)
-                .foregroundStyle(Color("Gold"))
-                .padding()
-                .background(Color("Maroon"))
-                .cornerRadius(15)
-            Spacer()
-            Text(messageString)
-                .font(.largeTitle)
-                .fontWeight(.heavy)
-                .minimumScaleFactor(0.5)
-                .multilineTextAlignment(.center)
-                .foregroundStyle(.red)
-                .frame(height: 150)
-                .frame(maxWidth: .infinity)
-                .border(.orange, width: 1)
-                .padding()
-            
-            Spacer()
-            
-            HStack {
-                Button("Awesome") {
-                    messageString = "You Are Awesome!"
-                }
-                .buttonStyle(.borderedProminent)
+        GeometryReader { geometry in
+            VStack {
                 Spacer()
-                Button("Great") {
-                    messageString = "You Are Great!"
+                Group {
+                    Text("hi")
+                    Text("hi")
+                    Text("hi")
+                    Text("hi")
+                    Text("hi")
                 }
-                .buttonStyle(.borderedProminent)
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                
+                Group {
+                    Text(messageString)
+                        .font(.largeTitle)
+                        .fontWeight(.heavy)
+                        .minimumScaleFactor(0.5)
+                        .multilineTextAlignment(.center)
+                        .foregroundStyle(.red)
+                        .frame(height: 150)
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                    
+                    Spacer()
+                    Divider()
+                        .background(.black)
+                        .padding()
+                        .frame(width: 150.0)
+                    Rectangle()
+                        .fill(.indigo)
+                        .frame(width: geometry.size.width*(0.5), height: 1)
+                }
+                HStack {
+                    Button("Awesome") {
+                        messageString = "You Are Awesome!"
+                    }
+                    .buttonStyle(.borderedProminent)
+                    Spacer()
+                    
+                    Button("Great") {
+                        messageString = "You Are Great!"
+                    }
+                    .buttonStyle(.borderedProminent)
+                }
+                .padding()
             }
-            .padding()
         }
     }
 }
