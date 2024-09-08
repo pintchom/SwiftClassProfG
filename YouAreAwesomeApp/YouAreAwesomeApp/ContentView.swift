438da693a801db3d8596b65bd98498dd566d5f1d
Max Pintchouk <maxpintchouk4321@gmail.com>
2024-09-08 17:15:27 -0400

    1.11

--- YouAreAwesomeApp/YouAreAwesomeApp/ContentView.swift
+++ YouAreAwesomeApp/YouAreAwesomeApp/ContentView.swift
@@ -9,41 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var messageString: String = ""
+    @State private var imageString: String = ""
     var body: some View {
         VStack {
-            Spacer()
             
-//            Image(systemName: "speaker.wave.2", variableValue: 0.0)
-//                .resizable()
-//                .scaledToFit()
-//                .symbolRenderingMode(.multicolor)
-//                .padding()
-//                .background(Color(hue: 0.557, saturation: 0.399, brightness: 0.528))
-//                .cornerRadius(30)
-//                .shadow(color: .red, radius: 100, x:20, y:20)
-//                .overlay(
-//                    RoundedRectangle(cornerRadius: 30)
-//                        .stroke(.red, lineWidth: 1)
-//                )
-//                .padding()
-            
-            Text(messageString)
-                .font(.largeTitle)
-                .fontWeight(.heavy)
-                .minimumScaleFactor(0.5)
-                .multilineTextAlignment(.center)
-                .foregroundStyle(.red)
-                .frame(height: 150)
-                .frame(maxWidth: .infinity)
+            Image(imageString)
+                .resizable()
+                .scaledToFit()
+                .cornerRadius(30)
+                .shadow(radius: 30)
                 .padding()
+            
+            
+//            Text(messageString)
+//                .font(.largeTitle)
+//                .fontWeight(.heavy)
+//                .minimumScaleFactor(0.5)
+//                .multilineTextAlignment(.center)
+//                .foregroundStyle(.red)
+//                .frame(height: 150)
+//                .frame(maxWidth: .infinity)
+//                .padding()
             
             Spacer()
             
             HStack {
-                Button("Show Message") {
-                    let message1 = "1"
-                    let message2 = "2"
-                    messageString = (messageString == message1 ? message2 : message1)
+                Button("Show Image") {
+                    imageString = (imageString == "image0" ? "image1" : "image0")
                 }
                 .buttonStyle(.borderedProminent)
             }
